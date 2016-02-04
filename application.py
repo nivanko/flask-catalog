7c9d3ae1dbe041bd931c336d85e87e3efb573c78
Nikolay Ivanko <nikolay.i@didww.com>
2016-02-04 20:14:49 +0200

    Add: GitHub login functionality (beta)

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
+import httplib2
+import json
 import os
-from flask import Flask, flash, jsonify, redirect, render_template, request, \
-                    send_from_directory, session, url_for
+import random
+import requests
+import string
+from flask import Flask, flash, jsonify, make_response, redirect, \
+                    render_template, request, send_from_directory, url_for
+from flask import session as login_session
 from werkzeug import secure_filename
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
@@ -9,7 +15,8 @@ from setup import Base, Category, Item
 # Define globals
 IS_LOGGED_IN = False
 IMAGES_DIRECTORY = 'images'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+CLIENT_ID = '88143cc4d646e722384a'
+CLIENT_SECRET = '896cbafa5d0ebed1a62c17502ef806e7a911a319'
 
 app = Flask(__name__)
 
@@ -73,7 +80,7 @@ def add_item():
                             description = request.form['description'],
                             picture = filename,
                             category_id = request.form['category_id'],
-                            creator = session['username'])
+                            creator = login_session['username'])
             db_session.add(new_item)
             db_session.commit()
             return redirect(url_for('list_categories'))
@@ -114,24 +121,75 @@ def edit_item(category_name, item_name):
         return redirect(url_for('login'))
 
 
-# Process user login
-@app.route('/login', methods=['GET', 'POST'])
+# Show login page
+@app.route('/login')
 def login():
     global IS_LOGGED_IN
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        IS_LOGGED_IN = True
-        return redirect(url_for('list_categories'))
-    else:
-        return render_template('login.html', is_login = IS_LOGGED_IN)
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                for x in xrange(32))
+    login_session['state'] = state
+    return render_template('login.html', client_id = CLIENT_ID, state = state,
+                            is_login = IS_LOGGED_IN)
+
+
+# Process GitHub login
+@app.route('/glogin')
+def github_login():
+    global IS_LOGGED_IN
+    #
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Stub begin
+    # Here we will check response & handle possible errors
+    state = login_session['state']
+    code = request.args.get('code')
+    # Stub end
+    # Get access token
+    url = 'https://github.com/login/oauth/access_token'
+    headers = {'Accept': 'application/json'}
+    params = { 'client_id' : CLIENT_ID, 'client_secret' : CLIENT_SECRET,
+                'code' : code, 'state' : state }
+    result = requests.post(url, data = params, headers = headers)
+    data = result.json()
+    login_session['access_token'] = data['access_token']
+    # Get user info
+    url = 'https://api.github.com/user'
+    token_param = 'token %s' % login_session['access_token']
+    headers = {'Authorization': token_param, 'Accept': 'application/json'}
+    result = requests.get(url, headers = headers)
+    data = result.json()
+    login_session['username'] = data['login']
+    login_session['fullname'] = data['name']
+    #
+    print login_session['username']
+    print login_session['fullname']
+    #
+    IS_LOGGED_IN = True
+    return redirect(url_for('list_categories'))
 
 
 # Log user out
 @app.route('/logout')
 def logout():
     global IS_LOGGED_IN
-    # remove the username from the session if its there
-    session.pop('username', None)
+    # Delete access token
+    url = 'https://api.github.com/applications/%s/tokens/%s' % (
+            CLIENT_ID, login_session['access_token'])
+    result = requests.delete(url, auth=(CLIENT_ID, CLIENT_SECRET))
+    # Stub begin
+    print result.headers
+    print result.text
+    # Stub end
+    # remove the user data from the session if its there
+    login_session.pop('access_token', None)
+    login_session.pop('state', None)
+    login_session.pop('username', None)
+    login_session.pop('fullname', None)
+    #
+    print login_session
+    #
     IS_LOGGED_IN = False
     return redirect(url_for('list_categories'))
 
